b826223f7e20621f876888a0b6d5ae08dbd37d38
202544004 <202544004@itc.ac.kr>
2025-09-29 11:48:19 +0900

    v0.2 types/main.go update

--- week06/types/mian.go
+++ week06/types/mian.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
 func main() {
+	var text string = "mao"
 	fmt.Println(math.Floor(2.75))
 	fmt.Println(strings.Title("hee lo"))
+	fmt.Println(strings.Title(text))
+
+	name := "mao mao mao"
+
+	fmt.Println(name, reflect.TypeOf(name))
 }
