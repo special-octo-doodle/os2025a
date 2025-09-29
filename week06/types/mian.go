cf00c55ac09f8abf3178a8ebd3ee30bc0c50b099
202544004 <202544004@itc.ac.kr>
2025-09-29 12:13:07 +0900

    v0.3 types/main.go update

--- week06/types/mian.go
+++ week06/types/mian.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"reflect"
-	"strings"
 )
 
 func main() {
-	var text string = "mao"
-	fmt.Println(math.Floor(2.75))
-	fmt.Println(strings.Title("hee lo"))
-	fmt.Println(strings.Title(text))
+	// var text string = "mao"
+	// fmt.Println(math.Floor(2.75))
+	// fmt.Println(strings.Title("hee lo"))
+	// fmt.Println(strings.Title(text))
 
-	name := "mao mao mao"
+	// name := "mao mao mao"
 
-	fmt.Println(name, reflect.TypeOf(name))
+	var a float64
+	var b string
+	var c int32
+	var d bool
+
+	fmt.Println(a, reflect.TypeOf(a))
+	fmt.Println(b, reflect.TypeOf(b))
+	fmt.Println(c, reflect.TypeOf(c))
+	fmt.Println(d, reflect.TypeOf(d))
 }
