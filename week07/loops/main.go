2d45aacff05df6df09866c574f6c2bcff6580869
202544004 <202544004@itc.ac.kr>
2025-10-13 10:39:12 +0900

    v0.15 week07 - CONVERSIONS fix

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -11,9 +11,9 @@ func main() {
 	fmt.Println("int", int(length)*width)
 	fmt.Println("float64", length*float64(width))
 
-	fmt.Println("int value:", reflect.TypeOf(width))
-	fmt.Println("float64 value:", reflect.TypeOf(length))
+	fmt.Println("int value:", reflect.TypeOf(width), width)
+	fmt.Println("float64 value:", reflect.TypeOf(length), length)
 
-	fmt.Println("int to float64 value:", reflect.TypeOf(float64(length)))
-	fmt.Println("float64 to int value:", reflect.TypeOf(int(width)))
+	fmt.Println("int to float64 value:", reflect.TypeOf(float64(length)), float64(length))
+	fmt.Println("float64 to int value:", reflect.TypeOf(int(width)), int(width))
 }
