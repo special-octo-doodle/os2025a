b0ce64426845860c8f1cce2872a15c942ba8f0dc
202544004 <202544004@itc.ac.kr>
2025-11-03 11:56:40 +0900

    week10 - array v0.2

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var arrayBool [3]bool
+	var arrayBool [3]bool = [3]bool{true, false, true}
 	var arrayint [3]int
 	fmt.Println(arrayBool[1])
-	arrayint[1]++
-	arrayint[1]++
+	arrayint[1] = 2
 	fmt.Println(arrayint[1])
 }
